bd3988f9842faa6b5c4acbcd9e03c8b8ba19596b
Daniel Gimenes <danielcgimenes@gmail.com>
2024-05-28 18:30:22 -0700

    support for bytecount with '-c' argument

--- gowc.go
+++ gowc.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+const NO_ARGS_SUPPLIED_ERR = "Error: no arguments were supplied."
+const INVALID_ARGUMENTS_ERR = "Error: invalid arguments."
+const CANT_OPEN_FILE_ERR = "Error: can't open file."
+const CANT_GET_FILE_INFO_ERR = "Error: can't get file info."
+
 func main() {
-	fmt.Println("Hello GO! This is GO WC!")
+	if len(os.Args) == 0 {
+		fmt.Println(NO_ARGS_SUPPLIED_ERR)
+		return
+	}
+
+	if len(os.Args) == 3 && os.Args[1] == "-c" {
+		filePath := os.Args[2]
+		// file, err := os.Open(filePath)
+		// if err != nil {
+		// 	fmt.Println(CANT_OPEN_FILE_ERR)
+		// }
+		fileInfo, err := os.Stat(filePath)
+		if err != nil {
+			fmt.Println(CANT_GET_FILE_INFO_ERR)
+		}
+		fmt.Println(fileInfo.Size(), fileInfo.Name())
+		return
+	} else {
+		fmt.Println(INVALID_ARGUMENTS_ERR)
+		return
+	}
+
 }
