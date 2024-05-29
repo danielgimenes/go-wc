854a3f1f307c71b37c623475dbffe5ce21464baf
Daniel Gimenes <danielcgimenes@gmail.com>
2024-05-28 18:42:38 -0700

    refactor error handling and command line arguments

--- gowc.go
+++ gowc.go
@@ -5,32 +5,44 @@ import (
 	"os"
 )
 
-const NO_ARGS_SUPPLIED_ERR = "Error: no arguments were supplied."
-const INVALID_ARGUMENTS_ERR = "Error: invalid arguments."
-const CANT_OPEN_FILE_ERR = "Error: can't open file."
-const CANT_GET_FILE_INFO_ERR = "Error: can't get file info."
+const NO_ARGS_SUPPLIED_ERR = "no arguments were supplied."
+const INVALID_ARGUMENTS_ERR = "invalid arguments."
+const CANT_OPEN_FILE_ERR = "can't open file."
+const CANT_GET_FILE_INFO_ERR = "can't get file info."
+const CLOSE_FILE_ERR = "can't close file"
+
+func exitWithError(errorMessage string) {
+	fmt.Println("Error:", errorMessage)
+	os.Exit(1)
+}
 
 func main() {
 	if len(os.Args) == 0 {
-		fmt.Println(NO_ARGS_SUPPLIED_ERR)
-		return
+		exitWithError(NO_ARGS_SUPPLIED_ERR)
 	}
 
-	if len(os.Args) == 3 && os.Args[1] == "-c" {
-		filePath := os.Args[2]
-		// file, err := os.Open(filePath)
-		// if err != nil {
-		// 	fmt.Println(CANT_OPEN_FILE_ERR)
-		// }
+	if len(os.Args) != 3 {
+		exitWithError(INVALID_ARGUMENTS_ERR)
+	}
+	operationArg := os.Args[1]
+	filePath := os.Args[2]
+
+	switch operationArg {
+	case "-c":
 		fileInfo, err := os.Stat(filePath)
 		if err != nil {
-			fmt.Println(CANT_GET_FILE_INFO_ERR)
+			exitWithError(CANT_GET_FILE_INFO_ERR)
 		}
 		fmt.Println(fileInfo.Size(), fileInfo.Name())
-		return
-	} else {
-		fmt.Println(INVALID_ARGUMENTS_ERR)
-		return
+	case "-l":
+		file, err := os.Open(filePath)
+		if err != nil {
+			exitWithError(CANT_OPEN_FILE_ERR)
+		}
+		closeErr := file.Close()
+		if closeErr != nil {
+			exitWithError(CLOSE_FILE_ERR)
+		}
 	}
 
 }
